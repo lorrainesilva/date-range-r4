9cd2289783aab8086d83dee93dfef6cc359466fe
João Victor Rodrigues da Silva <victor.joao.1611@gmail.com>
2018-07-04 19:37:01 -0300

    Initial commit

---
+++ app/src/main/java/reminder/daterange/br/unb/cic/reminders/view/FiltersListChangeListener.java
@@ -0,0 +1,8 @@
+package reminder.daterange.br.unb.cic.reminders.view;
+
+import reminder.daterange.br.unb.cic.reminders.controller.ReminderFilter;
+/*** added by dManageReminder
+ */
+public interface FiltersListChangeListener {
+	public void onSelectedFilterChanged(ReminderFilter filter);
+}
---
+++ app/src/main/java/reminder/daterange/br/unb/cic/reminders/view/InvalidHourException.java
@@ -0,0 +1,11 @@
+package reminder.daterange.br.unb.cic.reminders.view;
+
+import reminder.daterange.br.unb.cic.reminders.model.InvalidFormatException;
+/*** added by dManageReminder
+ */
+public class InvalidHourException extends InvalidFormatException {
+	private static final long serialVersionUID = 8341230773647025695L;
+	public InvalidHourException(Object o) {
+		super("This hour " + o);
+	}
+}
